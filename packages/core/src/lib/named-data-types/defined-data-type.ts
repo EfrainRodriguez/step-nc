f26e76e7f5068c0dbdcfdd9537146c62dabf0b2e
EfrainRodriguez <efrainrg92@gmail.com>
2023-12-09 11:53:03 -0500

    mapping TYPE from EXPRESS to Typescript type aliases

---
+++ packages/core/src/lib/named-data-types/defined-data-type.ts
@@ -0,0 +1,17 @@
+/**
+ * Defined data types are declared by type declarations (see 9.1). A defined data type is assigned
+ * a type identifier by the user. A defined data type is referenced by this identifier.
+ * @see ISO-10303-11:2004 8.3.2 Defined data type
+ */
+
+/**
+ * Defined data types from EXPRESS are mapped here to TypeScript type aliases using the keyword type.
+ * For example, the following EXPRESS code:
+ *
+ * TYPE volume = REAL;
+ * END_TYPE;
+ *
+ * is mapped to the following TypeScript code:
+ *
+ * type Volume = STPReal;
+ */
